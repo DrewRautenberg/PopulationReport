af490c76686190a58fc99e7452d8871b98ff4440
Drew Rautenberg <drew@drewrautenberg.com>
2017-10-18 14:51:37 -0400

    variables declared

--- src/PopulationReport.java
+++ src/PopulationReport.java
@@ -1,8 +1,45 @@
 // Author: Drew Rautenberg
 // Class section: CS1110-560, Wednesday 2:30 to 4:20
 // Program: Assignment 4, Population Report
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class PopulationReport {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String[] code;
+        String[] name;
+        String[] continent;
+        String[] region;
+        long[] landArea;
+        long[] population;
+        int[] lifeExpectancy;
+        String[] shortCode;
+
+        String codeRaw;
+        String nameRaw;
+        String continentRaw;
+        String regionRaw;
+        long landAreaRaw;
+        long populationRaw;
+        int lifeExpectancyRaw;
+        String shortCodeRaw;
+
+        File input = new File("CountryData8Col3Hdr.csv");
+        Scanner inFile = new Scanner(input);
+
+    }
+
+    static void doTable1() {
+
+    }
+
+    static void doTable2() {
+
+    }
+
+    static void doTable3() {
 
     }
 }
